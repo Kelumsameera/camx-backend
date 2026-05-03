92130f65af110610825e95d68281ee1b232986f1
Kelumsameera <sameerakelum16@gmail.com>
2026-05-03 17:49:09 +0530

    chore: integrate mongoose for MongoDB connection and handle connection events

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ import express from 'express';
 
 let mongoURI = "mongodb+srv://admin:admin123@cluster0.gsp1kmw.mongodb.net/?appName=Cluster0";
 
+const mongoose = require('mongoose');
+mongoose.connect(mongoURI);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('Connected successfully');
+});
+
+
 const app = express();
 
 app.listen(3000, () => {
