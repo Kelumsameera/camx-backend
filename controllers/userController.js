ae66ff2fdcb176295f5e5717b693014cbc808a0c
Kelumsameera <sameerakelum16@gmail.com>
2026-05-04 16:00:00 +0530

    feat: add JWT authentication for user login and enhance user creation process

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,58 +1,60 @@
 import User from "../models/users.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"; 
 
 export function createUser(req, res) {
+  const data = req.body;
+  const hashedPassword = bcrypt.hashSync(data.password, 10);
 
-    const data = req.body;
-    const hashedPassword = bcrypt.hashSync(data.password, 10);
-    
-    const user = new User(
-        {
-            email: data.email,
-            firstName: data.firstName,
-            lastName: data.lastName,
-            password: hashedPassword,
-            role: data.role
-        }
-    );
-    user.save().then(
-        ()=>{
-            res.json({
-                message: "User created successfully",
-            })
-        }
-    )
-        
+  const user = new User({
+    email: data.email,
+    firstName: data.firstName,
+    lastName: data.lastName,
+    password: hashedPassword,
+    role: data.role,
+  });
+  user.save().then(() => {
+    res.json({
+      message: "User created successfully",
+    });
+  });
 }
 
-
 export function loginUsers(req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
+  const email = req.body.email;
+  const password = req.body.password;
 
-    User.find({email: email}).then(
-        (users) => {
-            if(users[0] == null){
-                res.json({
-                    message: "User not found"
-                })
-            } else {
-                const user = users[0]
-                
-                const isPasswordCorrect = bcrypt.compareSync(password, user.password)
-                if(isPasswordCorrect){
-                    res.json({
-                        message: "Login successful",
-                        
-                    })
-                } else {
-                    res.json({
-                        message: "Incorrect password"
-                    })
-                }
-            }
-        }
-    )
+  User.find({ email: email }).then((users) => {
+    if (users[0] == null) {
+      res.json({
+        message: "User not found",
+      });
+    } else {
+      const user = users[0];
 
+      const isPasswordCorrect = bcrypt.compareSync(password, user.password);
 
+      if (isPasswordCorrect) {
+        const payload = {
+          email: user.email,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          role: user.role,
+          isEmailVerified: user.isEmailVerified,
+          image: user.image,
+        };
+
+        const token = jwt.sign(payload, "secretKey96$2025")
+
+        res.json({
+          message: "Login successful",
+            token: token,
+        });
+      } else {
+        res.json({
+          message: "Incorrect password",
+        });
+      }
+    }
+  });
 }
--- index.js
+++ index.js
@@ -1,24 +1,60 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import userRouter from "./routes/userRouter.js";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
 
 const app = express();
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-      console.log('Connected to DB'); 
-  })
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Connected to DB");
+    })
     .catch((error) => {
         console.log(error);
     });
-  app.use(express.json());
-  app.use("/users",userRouter);
+app.use(express.json());
+
+app.use(
+    (req, res, next) => {
+
+        const Authorizationheaders = req.header("Authorization");
+        if (Authorizationheaders != null) {
+            const token = Authorizationheaders.replace("Bearer ", "");
+            console.log(token);
+
+            jwt.verify(token, "secretKey96$2025",
+                (error, content) => {
+                    
+                    if (content == null){
+                        console.log("Invalid token");
 
 
-  
-  app.listen(3000, () => {
-          console.log('Server is running on port 3000');
-      });
+
+                    }else {
+                        console.log(content);
+
+                    }
+
+                }
+            )
+
+
+
+        }
+      
+
+
+        next();
+
+    }
+);
+
+app.use("/users", userRouter);
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
